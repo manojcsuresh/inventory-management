20f70c4b9c7ab63fa00c11d3b1dabb0d138ba3e0
Manoj C S <72264723+manojcsuresh@users.noreply.github.com>
2025-06-19 22:11:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://tranquil-blancmange-5781d6.netlify.app/", // ✅ exact Netlify URL
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
